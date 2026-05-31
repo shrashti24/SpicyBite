65c89fae7a3216f40d0ce7b1acebcd9e9db3bbab
shrasti.gupta <shrastigupta2801@gmail.com>
2026-05-31 09:36:53 +0530

    update

--- app/src/main/java/com/example/spicybite/StartActivity.kt
+++ app/src/main/java/com/example/spicybite/StartActivity.kt
@@ -1,21 +1,28 @@
 package com.example.spicybite
 
 import android.content.Intent
-import androidx.activity.enableEdgeToEdge
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.spicybite.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
-    private val binding: ActivityStartBinding by lazy{
+
+    private val binding: ActivityStartBinding by lazy {
         ActivityStartBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge() // optional
-        setContentView(binding.root)  // Your start layout
-        binding.btnNext.setOnClickListener{
-            startActivity(Intent(this, LoginActivity::class.java))
+
+        enableEdgeToEdge()this
+
+        window.statusBarColor = getColor(android.R.color.transparent)
+
+        setContentView(binding.root)
+
+        binding.btnNext.setOnClickListener {
+            startActivity(Intent(, LoginActivity::class.java))
             finish()
         }
     }
